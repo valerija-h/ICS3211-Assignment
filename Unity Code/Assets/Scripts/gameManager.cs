1ef27f2010ae7fe0d67c8050282bfd069f382c33
valerija <vholomjova@gmail.com>
2020-11-12 00:19:52 +0000

    Init

---
+++ Unity Code/Assets/Scripts/gameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Game States
+public enum GameState {S1,S2,S3,S4}
+
+public delegate void OnStateChangeHandler();
+
+public class gameManager : Object
+{
+    protected gameManager() { }
+    private static gameManager instance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public GameState gameState { get; private set; }
+
+    public static gameManager Instance
+    {
+        get
+        {
+            if (gameManager.instance == null)
+            {
+                gameManager.instance = new gameManager();
+                if (gameManager.instance != null)
+                {
+                    DontDestroyOnLoad(gameManager.instance);
+                }
+                }
+            return gameManager.instance;
+        }
+
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+    public void OnApplicationQuit()
+    {
+        gameManager.instance = null;
+    }
+
+}
---
+++ Unity Code/Assets/Scripts/part1.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class part1 : MonoBehaviour
+{
+
+    gameManager GM;
+
+    void Awake()
+    {
+        GM = gameManager.Instance;
+        GM.OnStateChange += HandleOnStateChange;
+        Debug.Log("Current game state when Awakes: " + GM.gameState);
+    }
+
+    void Start()
+    {
+        Debug.Log("Current game state when Starts: " + GM.gameState);
+        GM.SetGameState(GameState.S1);
+    }
+
+    public void HandleOnStateChange()
+    {
+        Debug.Log("Handling state change to: " + GM.gameState);
+        Invoke("LoadS1", 3f);
+    }
+
+    public void LoadS1()
+    {
+        Debug.Log("Loaded S1");
+        Debug.Log("Start game tied to chair");
+        //Invoke("LoadS2", 3f);
+        //or
+        Invoke("LoadS3",3f);
+        GM.SetGameState(GameState.S3);
+
+    }
+
+    public void LoadS2()
+    {
+        Debug.Log("Loaded S2");
+        Debug.Log("Do nothing.");
+        Invoke("LoadS3",3f);
+        GM.SetGameState(GameState.S3);
+
+        //or Invoke("LoadS2", 3f);
+    }
+
+
+    public void LoadS3()
+    {
+        Debug.Log("Loaded S3");
+        Debug.Log("Untied hands.");
+        Invoke("LoadS4", 3f);
+        GM.SetGameState(GameState.S4);
+
+        //or Invoke("LoadS5", 3f);
+        //or Invoke("LoadS6", 3f);
+        //or Invoke("LoadS7", 3f);
+
+    }
+
+    public void LoadS4()
+    {
+        Debug.Log("Loaded S4");
+        Debug.Log("Wardrobe interactions.");
+
+    }
+
+
+    public void LoadS5()
+    {
+        Debug.Log("Loaded S5");
+        Debug.Log("Door interaction.");
+    }
+
+
+    public void LoadS6()
+    {
+        Debug.Log("Loaded S6");
+        Debug.Log("Water interactions.");
+    }
+
+    public void LoadS7()
+    {
+        Debug.Log("Loaded S7");
+        Debug.Log("Walk around");
+    }
+
+
+
+
+
+
+
+}
